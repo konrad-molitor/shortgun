ffb7d657412e51d55047f4621d03b8c9170a1670
kmolitor <fwarrior.skm@gmail.com>
2019-12-03 14:31:57 +0500

    removed static files serving

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,5 +1,4 @@
-import { Router, static as exstatic } from "express";
-import serveStatic from "serve-static";
+import { Router } from "express";
 import { auth } from "../middleware/auth";
 import errorHandler from "../middleware/errorHandler";
 import addShortcut from "./addShortcut";
@@ -24,16 +23,5 @@ router.get("/s/:shortcut", (req, res) => shortcutRedirect(req, res));
 router.post("/a/signup", (req, res) => signup(req, res));
 
 router.post("/a/login", (req, res) => loginUser(req, res));
-
-// const serve = serveStatic("public", {
-//   dotfiles: "ignore",
-//   index: ["index.html"],
-//   redirect: false,
-// });
-
-// router.use((req, res, next) => serve(req, res, next));
-
-router.use("/static", exstatic("public/static", {redirect: false}));
-router.use("/", exstatic("public"));
 
 export default router;
